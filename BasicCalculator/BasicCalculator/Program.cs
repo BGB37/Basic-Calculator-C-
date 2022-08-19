112db2812c9d0509a0aaeda66cb757dbf0e6846d
Bekir Gülbey Başoğlu <94611659+BGB37@users.noreply.github.com>
2022-08-19 12:46:38 +0300

    Fixed some bugs.
    - The Program now can respond to invalid user inputs except num1 and num2. I will be working on it.
    - I tried to apply DRY. Created functions to not write the same code over and over again.

--- BasicCalculator/BasicCalculator/Program.cs
+++ BasicCalculator/BasicCalculator/Program.cs
@@ -2,96 +2,161 @@
 
 using System;
 
-    namespace BasicCalculator
+namespace BasicCalculator
 {
     class Calculator
     {
-        static int num1 = 0;
-        static int num2 = 0;
+        static double num1 = 0;   //double'a çevirdik.
+        static double num2 = 0;   //double'a çevirdik.
         static string option;
         static double result;
         static string goOn = "Y";
         static int count = 0;
-        
-        static void Main(string[] args)
+
+        static void introMsg()
         {
-            while(goOn == "Y")  // Added a loop if user wants to continue calculating
-            {   
-                if (count == 0) { 
-                Console.WriteLine("\t----------  ----------");
-                Console.Write("\tBGB's Basic Calculator");
-                Console.WriteLine(" :)");
-                Console.WriteLine("\t----------  ---------");
-                } 
-                Console.Write("\tEnter first number: ");
-                num1 = Convert.ToInt32(Console.ReadLine()); // If you enter decimals it doesn't work.
-                Console.WriteLine(num1.GetType());
-                Console.Write("\tEnter second number: ");
-                num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\t----------  ----------");
+            Console.Write("\tBGB's Basic Calculator");
+            Console.WriteLine(" :)");
+            Console.WriteLine("\t----------  ---------");
+        }
 
-                Console.WriteLine("\n\tSelect an option that you want to execute:");
-                Console.WriteLine("\n\t + : Add");
-                Console.WriteLine("\t - : Subtract");
-                Console.WriteLine("\t x : Multiply");
-                Console.WriteLine("\t / : Divide");
-                
-                option = Console.ReadLine();
-               
-                switch (option)
-                {
-                    case "+":
-                        Console.ForegroundColor = ConsoleColor.Blue;
-                        Console.WriteLine($"\tOption you entered is: {option}");
-                        result = num1 + num2;
-                        Console.WriteLine($"\t{num1} + {num2} = " + result);
-                        break;
+        static void getNumbers()
+        {
+            Console.Write("\tEnter first number: ");
+            num1 = Convert.ToDouble(Console.ReadLine()); 
+            Console.Write("\tEnter second number: ");
+            num2 = Convert.ToDouble(Console.ReadLine());
+        }
 
-                    case "-":
-                        Console.ForegroundColor = ConsoleColor.Blue;
-                        Console.WriteLine($"\tOption you entered is: {option}");
-                        result = num1 - num2;
-                        Console.WriteLine($"\t{num1} - {num2} = " + result);
-                        break;
+        static void showOptions()
+        {
+            Console.WriteLine("\n\tSelect an option that you want to execute:");
+            Console.WriteLine("\n\t + : Add");
+            Console.WriteLine("\t - : Subtract");
+            Console.WriteLine("\t x : Multiply");
+            Console.WriteLine("\t / : Divide");
 
-                    case "x":
-                        Console.ForegroundColor = ConsoleColor.Blue;
-                        Console.WriteLine($"\tOption you entered is: {option}");
-                        result = num1 * num2;
-                        Console.WriteLine($"\t{num1} x {num2} = " + result);
-                        break;
+        }
 
-                    case "/":
-                        Console.ForegroundColor = ConsoleColor.Blue;
-                        Console.WriteLine($"\tOption you entered is: {option}");
-                        result = num1 / num2;
-                        Console.WriteLine($"\t{num1} / {num2} = " + result);
-                        break;
+        static void getOption()
+        {
+            option = Console.ReadLine();
+        }
 
-                    default:
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine($"\tOption you entered is: {option}");
-                        Console.WriteLine("\tcharacter you entered is not defined.");
-                        break;
-                }
-                count++;
+        static void ifUndefinedOptionEntered()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"\tYou entered: {option}");
+            Console.WriteLine("\tCharacter you entered is not defined. You should enter from the list below. \n\tSelect an option that you want to execute:\n\t +: Add\n\t -: Subtract\n\t x: Multiply\n\t / : Divide");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+       /* static void SwitchCase()
+        {
+            switch (option)
+            {
+                case "+":
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    result = num1 + num2;
+                    Console.WriteLine($"\t{num1} + {num2} = " + result);
+                    break;
+
+                case "-":
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine($"\tOption you entered is: {option}");
+                    result = num1 - num2;
+                    Console.WriteLine($"\t{num1} - {num2} = " + result);
+                    break;
+
+                case "x":
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine($"\tOption you entered is: {option}");
+                    result = num1 * num2;
+                    Console.WriteLine($"\t{num1} x {num2} = " + result);
+                    break;
+            }
+        } */
+
+        static void doYouWantToContinue()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("\n\tDo you want to continue? Y: yes / N: no");
+            goOn = Console.ReadLine().ToUpper();
+            while (goOn != "Y" && goOn != "N")
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"\n\tYou entered: {goOn}" + ". You should enter \"Y\" or \"N\" ");
                 Console.ForegroundColor = ConsoleColor.White;
-                Console.WriteLine("\n\tDo you want to continue? Y : yes / N : no");
+                Console.WriteLine("\n\tDo you want to continue? Y: yes / N: no");
                 goOn = Console.ReadLine().ToUpper();
+            }
 
-                if (goOn != "Y" && goOn != "N") {
-                    Console.WriteLine($"You entered: {goOn}" + ". You should enter \"Y\" or \"N\" ");
-                        while (goOn != "Y" && goOn != "N")
+        }
+
+        static void showResult()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            //Console.WriteLine($"\n\tOption you entered is: {option}");
+            switch (option)
+            {
+                case "+":
+                    result = num1 + num2;
+                    Console.WriteLine($"\n\t{num1} {option} {num2} = {result}");
+                    break;
+
+                case "-":
+                    result = num1 - num2;
+                    Console.WriteLine($"\n\t{num1} {option} {num2} = {result}");
+                    break;
+
+                case "x":
+                    result = num1 * num2;
+                    Console.WriteLine($"\n\t{num1} {option} {num2} = {result}");
+                    break;
+
+                case "/":
+                    result = num1 / num2;
+                    Console.WriteLine($"\n\t{num1} {option} {num2} = {result}");
+                    break;
+                default:
+                    while (option != "+" && option != "-" && option != "x" && option != "/")
+                    {
+                        ifUndefinedOptionEntered();
+                        getOption();
+                        showResult();
+
+                    }
+                    //doYouWantToContinue();
+                    //getNumbers();
+                    //showOptions();
+                    //showResult();
+                    break;
+            }
+        }
+            
+        
+                static void Main(string[] args)
+            {
+                    while (goOn == "Y")  // Added a loop if user wants to continue calculating
+                    {
+                        if (count == 0)
                         {
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            Console.WriteLine("\n\tDo you want to continue? Y : yes / N : no");
-                            Console.ForegroundColor = ConsoleColor.White;
-                            goOn = Console.ReadLine().ToUpper();
+                            introMsg();
                         }
-                }
 
-            } 
+                         getNumbers();
+                         showOptions();
+                         getOption();
+                         showResult();
+                         doYouWantToContinue();
+                         count++;
+
+                }
+                Console.WriteLine("\n\tProgram Ended.");
+            }
+
         }
 
     }
 
-}
